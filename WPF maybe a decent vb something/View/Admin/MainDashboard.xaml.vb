1fb8b48d467c847c2992d59750d432694cd778bb
Osamahweiti <osama.hweiti@gmail.com>
2024-05-28 11:06:26 +0300

    Switching grids

--- WPF maybe a decent vb something/View/Admin/MainDashboard.xaml.vb
+++ WPF maybe a decent vb something/View/Admin/MainDashboard.xaml.vb
@@ -2,4 +2,13 @@
     Private Sub RadioButton_Checked(sender As Object, e As RoutedEventArgs)
 
     End Sub
+
+    Private Sub RadioButton_Checked_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+    Private Sub ShowEmployeeView(sender As Object, e As RoutedEventArgs)
+        Dim employee As New EmployeeView()
+        ContentGrid.Children.Clear()
+        ContentGrid.Children.Add(employee)
+    End Sub
 End Class
