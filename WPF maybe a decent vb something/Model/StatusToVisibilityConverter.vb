1f55b59b2a61212c3eede4f4335d4c61347d6dfb
Mohmmadshabrani <m7madsh206@gmail.com>
2024-05-29 08:31:45 +0300

    fuck off

---
+++ WPF maybe a decent vb something/Model/StatusToVisibilityConverter.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Globalization
+Public Class StatusToVisibilityConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+        Dim status As String = TryCast(value, String)
+        Dim condition As String = TryCast(parameter, String)
+
+        If Not String.IsNullOrEmpty(status) AndAlso Not String.IsNullOrEmpty(condition) Then
+            If status.Equals(condition, StringComparison.OrdinalIgnoreCase) Then
+                Return Visibility.Visible
+            End If
+        End If
+
+        Return Visibility.Collapsed
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New NotSupportedException()
+    End Function
+End Class
--- WPF maybe a decent vb something/View/Requests/RequestsView.xaml.vb
+++ WPF maybe a decent vb something/View/Requests/RequestsView.xaml.vb
@@ -2,14 +2,6 @@
 Imports System.Diagnostics.Metrics
 
 Public Class RequestsView
-    Public Sub ApproveBtn_Click(sender As Object, e As RoutedEventArgs)
-        Throw New NotImplementedException()
-    End Sub
-
-    Public Sub DenyBtn_Click(sender As Object, e As RoutedEventArgs)
-        Throw New NotImplementedException()
-    End Sub
-
     Public Sub SearchButton_Click(sender As Object, e As RoutedEventArgs)
         Throw New NotImplementedException()
     End Sub
@@ -21,5 +13,21 @@ Public Class RequestsView
         requestsDataGrid.ItemsSource = requests
     End Sub
 
+    Public Sub ApproveBtn_Click(sender As Object, e As RoutedEventArgs)
+        Dim request As Requests = CType(requestsDataGrid.SelectedItem, Requests)
+        request.Status = "Approved"
+        Dim requestRepo As New RequestRepository()
+        requestRepo.Update(request)
+        Dim requests As List(Of Requests) = requestRepo.GetAllRequests()
+        requestsDataGrid.ItemsSource = requests
+    End Sub
 
+    Public Sub DenyBtn_Click(sender As Object, e As RoutedEventArgs)
+        Dim request As Requests = CType(requestsDataGrid.SelectedItem, Requests)
+        request.Status = "Denied"
+        Dim requestRepo As New RequestRepository()
+        requestRepo.Update(request)
+        Dim requests As List(Of Requests) = requestRepo.GetAllRequests()
+        requestsDataGrid.ItemsSource = requests
+    End Sub
 End Class
